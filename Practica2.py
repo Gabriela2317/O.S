c60939d71e5538211ea52a7b834bfaab47d349ef
Gabriela2317 <78568177+Gabriela2317@users.noreply.github.com>
2022-03-24 21:53:23 -0600

    Create Practica2.py

---
+++ Practica2.py
@@ -0,0 +1,14 @@
+import sys
+
+print('Practica2')
+
+#define las variables independientes
+
+a =1.03
+b=503
+c=2** 2
+d=a*b
+
+##imprimir los valores de las variables
+
+print("a="+ str(a)+" b= "+str(b)+" c= "+str(c)+ " d= "+str(d))
